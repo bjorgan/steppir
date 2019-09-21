87c02879ebeae6d54899c808393767f7b64349fd
Asgeir Bjorgan <asgeirbj@samfundet.no>
2019-09-21 19:11:15 +0000

    Add setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='steppir',
+      version='1.0',
+      description='SteppIR serial controller',
+      author='Asgeir Bjorgan',
+      py_modules=['steppir'],
+     )
