9e9b3d3e4ec1cb399e56b1848595aa0c6c9b30e3
Curt Mills, WE7U <curt.we7u@gmail.com>
2021-06-24 14:07:49 -0700

    Updating the "Author" correctly with both names

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ from distutils.core import setup
 setup(name='steppir',
       version='1.1',
       description='SteppIR serial controller',
-      author='Curt Mills, WE7U',
+      author='Asgeir Bjorgan & Curt Mills',
       py_modules=['steppir'],
      )
